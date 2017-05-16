ba15ede8baf8696713200578adda4e82ae4869bf
ankhzet <ankhzet@gmail.com>
2017-05-17 02:54:41 +0300

    Perform fetching on server side

--- grip/scripts/src/Grip/Server/ServerConnector.ts
+++ grip/scripts/src/Grip/Server/ServerConnector.ts
@@ -1,26 +1,10 @@
 
 import { ServerConnector } from '../../core/client/ServerConnector';
-import { CacheAction, CachePacketData } from './actions/Cache';
-import { Cache, Cacher } from '../Client/Cacher';
 
 export class GripConnector extends ServerConnector {
 
 	constructor() {
 		super('grip');
-
-		this.on(CacheAction, this.cache.bind(this));
-	}
-
-	cache({ book }: CachePacketData) {
-		let cacher = new Cacher();
-
-		cacher.fetch({
-			tocURI: book.uri,
-			pattern: /\/(xray-|xray\/)/,
-			context: '.entry-content',
-		}).then((cache: Cache) => {
-			console.log('cached:', cache)
-		});
 	}
 
 }
