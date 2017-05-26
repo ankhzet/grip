ec33d83d30406b85ddc251ca6ece0e900f418159
Yegor Shovkun <ankhzet@gmail.com>
2017-05-26 13:13:55 +0300

    Refactoring: remove redundant class

--- grip/scripts/src/Grip/Server/ContentedClientsPool.ts
+++
@@ -1,7 +0,0 @@
-
-import { ClientsPool } from '../../core/parcel/ClientsPool';
-import { GripClient } from './GripClient';
-
-export class ContentedClientsPool extends ClientsPool<GripClient> {
-
-}
--- grip/scripts/src/Grip/Server/Server.ts
+++ grip/scripts/src/Grip/Server/Server.ts
@@ -3,13 +3,13 @@ import { Server } from '../../core/server/Server';
 import { PacketDispatcher } from '../../core/parcel/PacketDispatcher';
 import { GripClient } from './GripClient';
 import { GripActions } from './actions/GripActions';
-import { ContentedClientsPool } from './ContentedClientsPool';
+import { ClientsPool } from '../../core/parcel/ClientsPool';
 
 export class GripServer extends Server<GripClient> {
 	constructor() {
 		super('grip',
 			new PacketDispatcher(GripActions),
-			new ContentedClientsPool((port: chrome.runtime.Port) => {
+			new ClientsPool<GripClient>((port: chrome.runtime.Port) => {
 				return new GripClient(port);
 			})
 		);
