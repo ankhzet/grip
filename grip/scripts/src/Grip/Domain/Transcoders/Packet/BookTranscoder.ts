55eb60f4afb10e524fe36950e9d81e317b287342
Yegor Shovkun <ankhzet@gmail.com>
2017-05-26 16:57:04 +0300

    Fix: packet transcoder for book entity

--- grip/scripts/src/Grip/Domain/Transcoders/Packet/BookTranscoder.ts
+++ grip/scripts/src/Grip/Domain/Transcoders/Packet/BookTranscoder.ts
@@ -8,13 +8,6 @@ export class BookTranscoder implements TranscoderInterface<Book, {}> {
 	public encode(book: Book): any {
 		return ObjectUtils.transform(book, (value, prop) => {
 			switch (prop) {
-				case 'toc':
-					value = ObjectUtils.transform(value, (title, uri) => (
-						console.log('encoding', uri || "", btoa(uri || "")),
-						[title, btoa(uri || "")]
-					));
-					break;
-
 				case 'matchers':
 					value = value.code();
 					break;
@@ -27,17 +20,6 @@ export class BookTranscoder implements TranscoderInterface<Book, {}> {
 	public decode(data: any, target?: Book): Book {
 		return ObjectUtils.transform(data, (value, prop, has) => {
 			switch (prop) {
-				case 'toc':
-					value = ObjectUtils.transform(value, (title, uri) => {
-						try {
-							console.log('decoding', uri || "", atob(uri || ""));
-							return [title, atob(uri || "")];
-						} catch (e) {
-							return [title, uri];
-						}
-					});
-					break;
-
 				case 'matchers':
 					value = ObjectUtils.transform(value, (code, matcher) => (
 						[code, matcher]
