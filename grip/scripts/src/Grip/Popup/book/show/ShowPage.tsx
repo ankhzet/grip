da15e3162490737964924a6e1b030206b91c1f10
ankhzet <ankhzet@gmail.com>
2017-05-27 12:45:37 +0300

    Use radiogroup instead of tabs

--- grip/scripts/src/Grip/Popup/book/show/ShowPage.tsx
+++ grip/scripts/src/Grip/Popup/book/show/ShowPage.tsx
@@ -17,6 +17,7 @@ import { BookUIDelegateInterface } from '../delegates/BookUIDelegateInterface';
 import { Link } from 'react-router';
 import { Utils } from '../../../Client/Utils';
 import { TocInterface } from '../../../Domain/TocInterface';
+import { RadioGroup } from '../../../../components/radiogroup';
 
 interface TocListProps {
 	uid: string;
@@ -53,7 +54,12 @@ export interface ShowPageProps {
 	params: { id: string };
 }
 
-export class ShowPage extends React.Component<ShowPageProps, { book: Book }> {
+export interface ShowPageState {
+	book: Book;
+	matcher: string;
+}
+
+export class ShowPage extends React.Component<ShowPageProps, ShowPageState> {
 
 	static path(uid: string): string {
 		return `${BooksPage.PATH}/${uid}`;
@@ -64,15 +70,19 @@ export class ShowPage extends React.Component<ShowPageProps, { book: Book }> {
 
 		this.state = {
 			book: null,
+			matcher: null,
 		};
 	}
 
 	async pullBook(id: string) {
 		this.setState({
 			book: null,
+			matcher: null,
 		});
+
 		this.setState({
 			book: await this.props.manager.getOne(id),
+			matcher: Object.keys(Book.matchers).shift(),
 		});
 	}
 
@@ -85,7 +95,11 @@ export class ShowPage extends React.Component<ShowPageProps, { book: Book }> {
 	}
 
 	render() {
-		let book = this.state.book;
+		let {
+			book,
+			matcher,
+		} = this.state;
+		let matchers = Object.keys(Book.matchers);
 
 		return (book || null) && (
 			<Panel>
@@ -114,15 +128,15 @@ export class ShowPage extends React.Component<ShowPageProps, { book: Book }> {
 
 						<div className="form-group">
 							<div className="input-group col-xs-12">
-								<label className="col-xs-2 form-control-static">URL:</label>
-								<span className="col-xs-10 form-control-static">{ book.uri }</span>
+								<label className="col-xs-2">URL:</label>
+								<span className="form-control-static">{ book.uri }</span>
 							</div>
 						</div>
 
 						<div className="form-group">
 							<div className="input-group col-xs-12" data-toggle="collapse" data-target={ '#chapter-list-' + book.uid }>
-								<label className="col-xs-2 form-control-static">Chapters:</label>
-								<span className="col-xs-10 form-control-static">{ Object.keys(book.toc).length }</span>
+								<label className="col-xs-2">Chapters:</label>
+								<span className="form-control-static">{ Object.keys(book.toc).length }</span>
 							</div>
 							<div className="collapse collapsed col-xs-12" id={ 'chapter-list-' + book.uid }>
 								<TocList uid={ book.uid } toc={ book.toc } columns={ 4 } />
@@ -130,36 +144,37 @@ export class ShowPage extends React.Component<ShowPageProps, { book: Book }> {
 						</div>
 
 						<div className="form-group">
-							<div className="col-xs-12">
-								<ul className="nav nav-tabs">
-									{ Object.keys(Book.matchers).map((matcher, index) => (
-										<li className={ index ? '' : 'active' }>
-											<a href={ '#matcher-' + matcher } data-toggle="tab">{ matcher }</a>
-										</li>
-									)) }
-								</ul>
+							<div className="input-group col-xs-12">
+								<label className="col-xs-2">
+									Matchers:
+								</label>
 
-								<div className="tab-content">
-									{ Object.keys(Book.matchers).map((matcher, index) => (
-										<div className={ "tab-pane" + (index ? "" : " active") } id={ 'matcher-' + matcher }>
-											<CodeMirror
-												className="form-control-static"
-												value={ book.matchers.get(matcher) }
-												options={{
-													mode: 'javascript',
-													theme: 'base16-oceanicnext-dark',
-													lineNumbers: true,
-													indentWithTabs: true,
-													tabSize: 2,
-													readOnly: true,
-												}}
-											/>
-										</div>
+								<RadioGroup
+									name="matchers"
+									value={ matcher }
+									onChange={ (e: any) => this.setState({...this.state, matcher: e.target.value}) }
+								>
+									{ matchers.map((name) => (
+										<label className="form-control-static" style={{ paddingRight: '15px', }}>
+											<input type="radio" value={ name } /> { name }
+										</label>
 									)) }
-								</div>
+								</RadioGroup>
 							</div>
-						</div>
 
+							<CodeMirror
+								className="form-control-static col-xs-12"
+								value={ book.matchers.get(matcher) }
+								options={{
+									mode: 'javascript',
+									theme: 'base16-oceanicnext-dark',
+									lineNumbers: true,
+									indentWithTabs: true,
+									tabSize: 2,
+									readOnly: true,
+								}}
+							/>
+						</div>
 					</form>
 				</PanelBody>
 
