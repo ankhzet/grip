f084e7934d3814c4548395d0da793bb1990b486a
Yegor Shovkun <ankhzet@gmail.com>
2017-05-31 12:46:05 +0300

    Add simple js perfmon

--- grip/scripts/src/background.ts
+++ grip/scripts/src/background.ts
@@ -1,9 +1,13 @@
 
 import { Grip } from './Grip/Grip';
+import { Nyan } from './core/probe/probe';
 
 ((sandbox) => {
 
 	console.log('Grip sandbox:', sandbox);
+
+	(<any>window).Nyan = Nyan;
+	(<any>window).Grip = sandbox;
 
 })(new Grip());
 
