8bcfc5f04e1d8b117c6f5ca0ec8b1d3729189300
Yegor Shovkun <ankhzet@gmail.com>
2017-06-01 11:31:54 +0300

    Update Probe interface

--- grip/scripts/src/core/probe/probe.ts
+++ grip/scripts/src/core/probe/probe.ts
@@ -1,41 +1,63 @@
 
-let probe = (f, times) => {
-	let n = +new Date(), got;
+let probe = ({ test, setup, step}, times) => {
+	let got = 0;
 
-	for (let i = 0; i < times; i++) f();
+	for (let i = 0; i < times; i++) {
+		let data = setup(step);
 
-	got = (+new Date()) - n;
+		let n = +new Date();
+		let processed = test(data);
+		got += (+new Date()) - n;
+
+		setup(data, processed || true);
+	}
 
 	return [got, got/ times];
 };
 
 export let Nyan = ({ tests, steps, probes }) => {
 	let statistics = {};
+	let from = +new Date();
 
-	for (let length of steps) {
+	for (let step of steps) {
+		for (let name of Object.keys(tests)) {
+			let { test, setup } = tests[name];
+			let chunk = (statistics[name] || (statistics[name] = {}));
 
-		let sample = (new Array(length)).join('.').split('.').map(() => ~~(Math.random() * 899 + 100));
-
-		for (let test of Object.keys(tests)) {
-			let [total, avg] = probe(tests[test](sample), probes);
-
-			let chunk = statistics[test] || (statistics[test] = {});
-			chunk[length] = [avg, total];
+			chunk[step] = probe({
+				test,
+				setup,
+				step,
+			}, probes);
 		}
 	}
+	let to = +new Date();
 
 	Object.defineProperty(statistics, 'describe', {
 		enumerable: false,
+
 		value: function (callback) {
 			let rows = [];
 			for (let test of Object.keys(this)) {
 				rows.push(`Test: ${test}`);
+				let steps = Object.keys(this[test]).map(Number).sort();
 
-				for (let length of Object.keys(this[test])) {
-					let sample = this[test][length];
-					rows.push(`\t${length} samples: ${sample[1]}/${probes} msec\n\t\tavg: ${sample[0]}`);
+				for (let step of steps) {
+					let sample = this[test][step];
+					rows.push(`\t${step} samples: ${sample[1]}/${probes} msec\n\t\tavg: ${sample[0]}`);
 				}
+
+				let degradation = [];
+				let current = 1, i = 0, step;
+
+				while (step = steps[i++]) {
+					degradation.push(step / current);
+					current = step;
+				}
+
+				rows.push(`\tDegradation: ${degradation.map((v, i) => i ? 'x' + v : v).join('->')}`);
 			}
+			rows.push(`Total: ${to - from} msec`);
 
 			return callback ? rows.map(callback) : rows;
 		},
